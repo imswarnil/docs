74bd5a407bb10cfed7f31b73d84d494ae232a4bd
Swarnil Singhai <31561157+imswarnil@users.noreply.github.com>
2025-11-03 11:35:52 -0800

    as

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -16,7 +16,29 @@ useSeoMeta({
   ogImage: 'https://ui.nuxt.com/assets/templates/nuxt/docs-light.png',
   twitterImage: 'https://ui.nuxt.com/assets/templates/nuxt/docs-light.png'
 })
+const links = ref([
+  {
+    label: 'Get started',
+    to: '/docs/getting-started',
+    icon: 'i-lucide-square-play'
+  },
+  {
+    label: 'Learn more',
+    to: '/docs/getting-started/theme/design-system',
+    color: 'neutral',
+    variant: 'subtle',
+    trailingIcon: 'i-lucide-arrow-right'
+  }
+])
 </script>
+
+<template>
+  <UPageHero
+    title="Ultimate Vue UI library"
+    description="A Nuxt/Vue-integrated UI library providing a rich set of fully-styled, accessible and highly customizable components for building modern web applications."
+    :links="links"
+  />
+</template>
 
 <template>
   <ContentRenderer
