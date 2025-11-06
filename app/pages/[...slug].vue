6a5d1832564a42b9ae3772037de51512539340b7
Swarnil Singhai <31561157+imswarnil@users.noreply.github.com>
2025-11-06 18:24:28 +0000

    asas

--- app/pages/[...slug].vue
+++ app/pages/[...slug].vue
@@ -1,7 +1,4 @@
-<!-- pages/docs/[...slug].vue -->
 <script setup lang="ts">
-// All of your existing script setup logic remains the same.
-// ...
 import type { ContentNavigationItem } from '@nuxt/content'
 import { findPageHeadline } from '@nuxt/content/utils'
 
@@ -11,61 +8,96 @@ const route = useRoute()
 const { toc } = useAppConfig()
 const navigation = inject<Ref<ContentNavigationItem[]>>('navigation')
 
-const { data: page } = await useAsyncData(route.path, () => queryContent().where({ _path: route.path }).findOne())
+const { data: page } = await useAsyncData(route.path, () => queryCollection('docs').path(route.path).first())
 if (!page.value) throw createError({ statusCode: 404, statusMessage: 'Page not found', fatal: true })
 
-const { data: surround } = await useAsyncData(`${route.path}-surround`, () => queryContent().only(['_path', 'title', 'description']).findSurround(route.path))
+const { data: surround } = await useAsyncData(`${route.path}-surround`, () => {
+  return queryCollectionItemSurroundings('docs', route.path, { fields: ['description'] })
+})
 
 const title = page.value.seo?.title || page.value.title
 const description = page.value.seo?.description || page.value.description
 
 useSeoMeta({ title, ogTitle: title, description, ogDescription: description })
 
-const headline = computed(() => findPageHeadline(page.value))
+const headline = computed(() => findPageHeadline(navigation?.value, page.value?.path))
 defineOgImageComponent('Docs', { headline: headline.value })
 </script>
 
 <template>
-  <div>
-    <!-- Ad #1: A standard horizontal banner before the main page content -->
-    <ClientOnly>
-      <GoogleAd variant="horizontal" />
-    </ClientOnly>
+ 
+ <ClientOnly>
+      <GoogleAd variant="horizontal" :refresh-key="route.path" />
+  </ClientOnly>
 
-    <UPage v-if="page">
-      <UPageHeader :title="page.title" :description="page.description" :headline="headline">
-        <template #links>
-          <UButton
-            v-for="(link, index) in page.links"
-            :key="index"
-            v-bind="link"
-          />
-        </template>
-      </UPageHeader>
-
-      <UPageBody>
-        <ContentRenderer :value="page" />
-
-        <USeparator v-if="surround?.length" class="my-8" />
-        <UContentSurround :surround="surround" />
-        
-        <!-- Ad #2: Multiplex ad at the very bottom of the page body -->
-        <ClientOnly>
-          <GoogleAd variant="multiplex" />
-        </ClientOnly>
-      </UPageBody>
-
-      <!-- Right Sidebar TOC + Ads -->
-      <template v-if="page.body?.toc?.links?.length" #right>
-        <UContentToc :title="toc?.title" :links="page.body.toc.links">
-          <template #bottom>
-            <!-- Ad #3: Vertical ad, perfect for a sticky sidebar -->
-            <ClientOnly>
-              <GoogleAd variant="vertical" />
-            </ClientOnly>
-          </template>
-        </UContentToc>
+  <UPage v-if="page">
+    <UPageHeader :title="page.title" :description="page.description" :headline="headline">
+      <template #links>
+        <UButton
+          v-for="(link, index) in page.links"
+          :key="index"
+          v-bind="link"
+        />
+        <PageHeaderLinks />
       </template>
-    </UPage>
-  </div>
+    </UPageHeader>
+
+    <ClientOnly>
+      <GoogleAd variant="rectangle" :refresh-key="route.path" />
+    </ClientOnly>
+    <UPageBody>
+      <!-- (3) In-article (fluid) before content -->
+      <ClientOnly>
+        <GoogleAd variant="in-article" />
+      </ClientOnly>
+
+      <ContentRenderer v-if="page" :value="page" />
+
+      <!-- (4) In-article between content & surround -->
+      <ClientOnly>
+        <GoogleAd variant="in-article" />
+      </ClientOnly>
+
+      <USeparator v-if="surround?.length" />
+
+      <!-- (5) Multiplex at bottom of body -->
+      <ClientOnly>
+        <GoogleAd variant="multiplex" />
+      </ClientOnly>
+
+      <UContentSurround :surround="surround" />
+    </UPageBody>
+
+    <!-- Right Sidebar TOC + Ads -->
+    <template v-if="page?.body?.toc?.links?.length" #right>
+      <UContentToc :title="toc?.title" :links="page.body?.toc?.links">
+        <template v-if="toc?.bottom" #bottom>
+          <div class="hidden lg:block space-y-6" :class="{ '!mt-6': page.body?.toc?.links?.length }">
+            <USeparator v-if="page.body?.toc?.links?.length" type="dashed" />
+            <UPageLinks :title="toc.bottom.title" :links="[]" />
+          </div>
+
+          <!-- (6) Square (responsive, clamped to 250px) -->
+          <ClientOnly>
+            <GoogleAd variant="square" />
+          </ClientOnly>
+
+          <!-- (7) Strict 250x250 -->
+          <ClientOnly>
+            <GoogleAd variant="square-fixed" />
+          </ClientOnly>
+
+          <!-- (8) Vertical (responsive) -->
+          <ClientOnly>
+            <GoogleAd variant="vertical" />
+          </ClientOnly>
+        </template>
+      </UContentToc>
+    </template>
+  </UPage>
+
+  <ClientOnly>
+      <GoogleAd variant="multiplex" :refresh-key="route.path" />
+ </ClientOnly>
+
 </template>
